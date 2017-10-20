c87f8a0962cfd99731222a10ede758f73eb6fdd3
yourname <m_itko@dir.bg>
2017-10-20 18:16:53 +0300

    w

---
+++ script.js
@@ -0,0 +1,71 @@
+//beggining of dmg area
+  function KDmg (){
+ var kdmg =  parseInt(document.getElementById("atk_k").value);  
+ var ddef = parseInt(document.getElementById("def_d").value); 
+return kdmg - ddef;
+  }
+  function DDmg () {
+var ddmg =  parseInt(document.getElementById("atk_d").value);  
+ var kdef = parseInt(document.getElementById("def_k").value); 
+ return ddmg - kdef; 
+  }
+  //end of dmg area 
+
+  //beggining of randomizer area 
+
+function Krandom(){
+ return Math.floor(Math.random() * 100) + 1 ;
+}    
+
+function Dradnom (){
+  return Math.floor (Math.random() * 100) + 1 ;
+}
+
+// end of randomizer area
+
+
+// begginging of dodge chance area   
+function Kctd () {
+   var ctd_K = parseInt (document.getElementById("ctd_k").value); 
+   var Kh = parseInt (document.getElementById("hp_d").value);
+   var one  = 100 - ctd_K ; 
+   var two = 100 * one; 
+   var three = two / 100 ; 
+   var result ; 
+     
+ if (Krandom() > three) {
+          result = o ; 
+   }  else {
+   result =  Kh - DDmg(); 
+   }
+    
+return result ;
+}
+
+
+
+function Dctd () { 
+
+ var ctd_D = parseInt (document.getElementById("ctd_d").value); 
+ var Dh = parseInt (document.getElementById("hp_k").value);
+ var one = 100 - ctd_D ; 
+ var two = 100 * one ; 
+ var three = two / 100; 
+ var result; 
+
+      
+ if (Drandom() > two) {
+          result = o ; 
+   }  else {
+   result =  Dh - KDmg(); 
+   }
+    
+return result ;
+}
+
+// end of chance area 
+
+
+  
+console.log("I love Darina");
+document.getElementById("demo").innerHTML = Kctd();
